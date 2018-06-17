6078fd93a0f57602588a79ae6c37bdd5a56fa74e
Carsten Klein <trancesilken@gmail.com>
2018-06-17 18:27:25 +0200

    fix: #14 refine typescript typings
    update typescript dependency to 2.8.3
    add missing typings tests
    add missing toJSON() method to typings

--- test/typings.ts
+++ test/typings.ts
@@ -12,3 +12,17 @@ e = new AssertionError(str, {a:1, b:2}, foo);
 const assertionError: AssertionError<{ bar: number }> = new AssertionError("msg", { bar: 42 });
 const msg: string = assertionError.message;
 const bar: number = assertionError.bar;
+
+class DerivedAssertionError<T = {}> extends AssertionError<T> {
+
+  public constructor(message, props?: T, ssf?: Function) {
+
+    super(message, props, ssf);
+  }
+}
+
+const errors: Error[] = [];
+errors.push(new AssertionError("msg"));
+errors.push(new AssertionError<{ bar: number }>("msg", { bar: 42 }));
+errors.push(new DerivedAssertionError<{ bar: number }>("msg", { bar: 42 }));
+
