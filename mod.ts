43612339a751e57d5f1af2dd18fc0a5f0801bb6c
Keith Cirkel <keithamus@users.noreply.github.com>
2021-09-28 10:07:30 +0100

    feat: assertionresult and rename (#27)
    * feat: add AssertionResult
    
    * refactor: rename index to mod.ts
    
    This is a Deno convention
    
    * Apply suggestions from code review
    
    Co-authored-by: Kristján Oddsson <koddsson@gmail.com>
    
    * refactor: remove unused ssf arg
    
    * refactor: remove ok/error factories
    
    * fix: add this.name back toJSON
    
    * test: add AssertionResult tests
    
    Co-authored-by: Kristján Oddsson <koddsson@gmail.com>

---
+++ mod.ts
@@ -0,0 +1,80 @@
+const canElideFrames = 'captureStackTrace' in Error
+const startStackFrames = new WeakMap()
+
+interface Result {
+  name: 'AssertionError' | 'AssertionResult'
+  ok: boolean
+  toJSON(...args: unknown[]): Record<string, unknown>
+}
+
+export class AssertionError<T> extends Error implements Result {
+  [key: string]: unknown
+
+  get name(): 'AssertionError' {
+    return 'AssertionError'
+  }
+
+  get ok() {
+    return false
+  }
+
+  constructor(public message = 'Unspecified AssertionError', props?: T, ssf?: Function) {
+    super(message)
+    if (canElideFrames && ssf) startStackFrames.set(this, ssf)
+    for (const key in props) {
+      if (!(key in this)) {
+        // @ts-ignore
+        this[key] = props[key];
+      }
+    }
+  }
+
+  get stack() {
+    if (canElideFrames) {
+      return (Error as any).captureStackTrace(this, startStackFrames.get(this) || AssertionError);
+    } else {
+      return super.stack
+    }
+  }
+
+  toJSON(stack: boolean): Record<string, unknown> {
+    return {
+      ...this,
+      name: this.name,
+      message: this.message,
+      ok: false,
+      // include stack if exists and not turned off
+      stack: stack !== false ? this.stack : undefined,
+    }
+  }
+
+}
+
+export class AssertionResult<T> implements Result {
+  [key: string]: unknown
+
+  get name(): 'AssertionResult' {
+    return 'AssertionResult'
+  }
+
+  get ok() {
+    return true
+  }
+
+  constructor(props?: T) {
+    for (const key in props) {
+      if (!(key in this)) {
+        // @ts-ignore
+        this[key] = props[key];
+      }
+    }
+  }
+
+  toJSON(): Record<string, unknown> {
+    return {
+      ...this,
+      name: this.name,
+      ok: this.ok,
+    }
+  }
+}
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
  * Include lib
  */
 
-import AssertionError from '../dist/index.js'
+import {AssertionError, AssertionResult} from '../dist/mod.js'
 
 /*!
  * Simple test runner.
@@ -20,10 +20,10 @@ function assert (pass, msg) {
   if (!pass) throw new Error(msg);
 }
 
-function suite(fn) {
+function suite(name, fn) {
   fn(test, assert);
 
-  console.log('');
+  console.log(name);
   console.log('  Tests (%d)', tests.length);
 
   tests.forEach(function (test) {
@@ -49,19 +49,20 @@ function suite(fn) {
   });
 
   console.log('');
-  process.exit(failures.length);
+  if (failures.length) process.exit(failures.length);
 };
 
 /*!
  * Load the tests
  */
 
-suite(function (test, assert) {
+suite('AssertionError', function (test, assert) {
   test('construction', function () {
     var err = new AssertionError();
     assert(err instanceof Error, 'instanceof Error');
     assert(err instanceof AssertionError, 'instanceof AssertionError');
     assert(err.name && err.name === 'AssertionError', 'name === "AssertionError"');
+    assert(err.ok === false, 'has ok=false prop');
   });
 
   test('message', function () {
@@ -78,12 +79,14 @@ suite(function (test, assert) {
   test('custom properties', function () {
     var err = new AssertionError('good message', {
         name: 'ShouldNotExist'
+      , ok: true
       , hello: 'universe'
       , message: 'bad message'
       , stack: 'custom stack'
     });
 
     assert(err.name === 'AssertionError', 'does not overwrite name');
+    assert(err.ok === false, 'does not overwrite ok');
     assert(err.message === 'good message', 'does not overwrite message');
     assert(err.hello && err.hello === 'universe', 'has custom property');
 
@@ -102,6 +105,7 @@ suite(function (test, assert) {
     var json = err.toJSON();
 
     assert(json.name === 'AssertionError', 'json has name');
+    assert(json.ok === false, 'json has ok');
     assert(json.message === 'some message', 'json has message');
     assert(json.hello === 'universe' && json.goodbye === 'known', 'json has custom properties');
 
@@ -112,5 +116,54 @@ suite(function (test, assert) {
 
     var nostack = err.toJSON(false);
     assert(!nostack.stack, 'no stack on false argument');
+  });
+});
+
+suite('AssertionResult', function (test, assert) {
+  test('construction', function () {
+    var res = new AssertionResult();
+    assert(res instanceof Error === false, 'not instanceof Error');
+    assert(res instanceof AssertionResult, 'instanceof AssertionResult');
+    assert(res.name && res.name === 'AssertionResult', 'name === "AssertionResult"');
+    assert(res.ok === true, 'has ok=true prop');
+  });
+
+  test('message', function () {
+    var res = new AssertionResult('Oops.')
+    assert('message' in res === false, 'has no message prop');
+  });
+
+  test('stack', function() {
+    var res = new AssertionResult()
+    assert('stack' in res === false, 'has no stack');
+  });
+
+  test('custom properties', function () {
+    var res = new AssertionResult({
+        name: 'ShouldNotExist'
+      , ok: false
+      , hello: 'universe'
+      , message: 'good message'
+      , stack: 'custom stack'
+    });
+
+    assert(res.name === 'AssertionResult', 'does not overwrite name');
+    assert(res.ok === true, 'does not overwrite ok property');
+    assert(res.message === 'good message', 'can overwrite message');
+    assert(res.hello && res.hello === 'universe', 'has custom property');
+    assert(res.stack && res.stack === 'custom stack', 'can overwrite stack');
+  });
+
+  test('.toJSON()', function () {
+    var res = new AssertionResult({
+        hello: 'universe'
+      , goodbye: 'known'
+    });
+
+    var json = res.toJSON();
+
+    assert(json.name === 'AssertionResult', 'json has name');
+    assert(json.ok === true, 'json has ok');
+    assert(json.hello === 'universe' && json.goodbye === 'known', 'json has custom properties');
   });
 });
